f27f80d3d2d60106e9a1a65a4bd3ac2c6515ce65
DefGh <defghq@gmail.com>
2023-10-24 16:32:21 +0400

    v3

---
+++ Tests/Tests.cs
@@ -0,0 +1,17 @@
+using PandaTech.IEnumerableFilters;
+
+namespace Tests;
+
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
