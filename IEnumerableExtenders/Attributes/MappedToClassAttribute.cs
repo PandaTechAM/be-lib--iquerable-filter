b8a8391f869b1f4e9bfc884f25072703b813a182
DefGh <defghq@gmail.com>
2023-10-24 16:31:41 +0400

    added filtering attribute

---
+++ IEnumerableExtenders/Attributes/MappedToClassAttribute.cs
@@ -0,0 +1,32 @@
+﻿using PandaTech.IEnumerableFilters.Dto;
+
+namespace PandaTech.IEnumerableFilters.Attributes;
+
+[AttributeUsage(AttributeTargets.Class)]
+public class MappedToClassAttribute : Attribute
+{
+    public readonly Type? TargetType;
+    
+    public MappedToClassAttribute(Type type)
+    {
+        TargetType = type;
+    }
+}
+
+[AttributeUsage(AttributeTargets.Property)]
+public class MappedToPropertyAttribute : Attribute
+{
+    public Type? FilterType;
+    
+    public Type? TargetConverterType;
+    public Type? BackwardConverterType;
+    public readonly string TargetPropertyName;
+    public ComparisonType[]? ComparisonTypes;
+
+    public MappedToPropertyAttribute(string property)
+    {
+        TargetPropertyName = property;
+    }
+}
+
+
