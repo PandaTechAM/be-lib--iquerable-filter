5a676ca39079fe501981f16398155dd17e79cc93
Ruben Bisharyan <ruben.bisharyan@pandatech.it>
2024-04-08 18:54:18 +0400

    Fix ENUM distinct column values for ENUM

--- test/EFCoreQueryMagic.Test/Enums/PaymentStatus.cs
+++ test/EFCoreQueryMagic.Test/Enums/PaymentStatus.cs
@@ -1,8 +1,11 @@
-﻿namespace EFCoreQueryMagic.Test.Enums;
+﻿using EFCoreQueryMagic.Attributes;
+
+namespace EFCoreQueryMagic.Test.Enums;
 
 public enum PaymentStatus
 {
     Pending,
     Completed,
+    [HideEnumValue]
     Failed
 }
